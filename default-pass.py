807516a19bfb712ea6a21429cf2380ba361261a4
Andrew Mass <amass1212@gmail.com>
2016-04-23 20:16:02 -0500

    WIP. Add command line interface to default-pass.py.

--- default-pass.py
+++ default-pass.py
@@ -1,42 +1,69 @@
 from bs4 import BeautifulSoup
 import urllib
 import urllib2
+import sys
+
+print sys.argv
 
 creds = {};
-output = open('./default-creds.csv', 'w')
+credfile = open('./default-creds.csv', 'r+')
 
-# Get list of router manufacturers
-manufct_list_source = urllib.urlopen('http://www.routerpasswords.com').read()
-manufct_list_soup = BeautifulSoup(manufct_list_source, "html.parser")
-for child in manufct_list_soup.find_all('select')[0].contents:
-    if child.name == 'option':
-        value = child['value']
-        if value not in creds:
-            creds[value] = []
+# Print the help text
+if '--help' in sys.argv:
+    print 'Usage: python default-pass.py [Manufacturer]...'
 
-print('Found %d manufacturers' % len(creds))
+# Reload the credential file
+if '--reload' in sys.argv:
 
-# Loop through each manufacturer and collect all username/password combinations
-count = 0
-for manufct in creds:
-    count += 1;
-    print('Scanning %d / %d: %s' % (count, len(creds), manufct) )
+    # Get list of router manufacturers
+    manufct_list_source = urllib.urlopen('http://www.routerpasswords.com').read()
+    manufct_list_soup = BeautifulSoup(manufct_list_source, "html.parser")
+    for child in manufct_list_soup.find_all('select')[0].contents:
+        if child.name == 'option':
+            value = child['value']
+            if value not in creds:
+                creds[value] = []
 
-    form_data = {'findpass': 1, 'router': manufct, 'findpassword': 'Find Password'}
-    data = urllib.urlencode(form_data)
-    cred_list_req = urllib2.Request('http://www.routerpasswords.com', data)
-    cred_list_source = urllib2.urlopen(cred_list_req).read()
-    cred_list_soup = BeautifulSoup(cred_list_source, "html.parser")
+    print('Found %d manufacturers' % len(creds))
 
-    # Loop through table results and store username/password combinations
-    for row in cred_list_soup.find_all('tr'):
-        if len(row.find_all('td')) == 5:
-            username = row.find_all('td')[3].string
-            password = row.find_all('td')[4].string
-            if (username, password) not in creds[manufct]:
-                creds[manufct].append((username, password))
-                output.write('%s,%s,%s\n' % (manufct, username, password))
-                output.flush()
+    # Loop through each manufacturer and collect all username/password combinations
+    count = 0
+    for manufct in creds:
+        credfile.write('%s\n' % manufct)
+        count += 1;
+        print('Scanning %d / %d: %s' % (count, len(creds), manufct) )
 
-output.close()
+        form_data = {'findpass': 1, 'router': manufct, 'findpassword': 'Find Password'}
+        data = urllib.urlencode(form_data)
+        cred_list_req = urllib2.Request('http://www.routerpasswords.com', data)
+        cred_list_source = urllib2.urlopen(cred_list_req).read()
+        cred_list_soup = BeautifulSoup(cred_list_source, "html.parser")
+
+        # Loop through table results and store username/password combinations
+        for row in cred_list_soup.find_all('tr'):
+            if len(row.find_all('td')) == 5:
+                username = row.find_all('td')[3].string
+                password = row.find_all('td')[4].string
+                if (username, password) not in creds[manufct]:
+                    creds[manufct].append((username, password))
+                    credfile.write('%s,%s,%s\n' % (manufct, username, password))
+                    credfile.flush()
+
+# Return to the beginning of the file
+credfile.seek(0)
+
+# Clear the credential file if the user requests it
+if '--clear' in sys.argv:
+    credfile.truncate()
+else:
+    if credfile.readline() == '':
+        print 'EMPTY! You should probably run with `--reload`'
+    else:
+        for query in sys.argv:
+            if query != 'default-pass.py' and query != '--clear' and query != '--reload' and query != '--help':
+                print query
+
+
+# Close the credential file
+credfile.close()
 
