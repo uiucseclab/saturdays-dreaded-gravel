af20395a1d72ad9c9f7846cdb9280bf1f8fd3053
root <root@localhost.localdomain>
2016-05-02 21:50:17 +0000

    modified airodump 2 to work with given args

--- airodump2.sh
+++ airodump2.sh
@@ -1,1 +1,1 @@
-airodump-ng -c 1 --bssid 00:0C:E6:22:BA:67 -w dump $1 #eventually BSSID and Channel will be part of input
+airodump-ng -c $3 --bssid $2 -w dump $1 #eventually BSSID and Channel will be part of input
